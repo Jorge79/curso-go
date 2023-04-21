72edce9d257232eb4a635a0a80d97c689883f994
Jorge79 <Jorge7.9@hotmail.com>
2023-04-21 20:02:43 -0300

    compor templates

---
+++ cap2/templates/5/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"os"
+	"text/template"
+)
+
+type Curso struct {
+	Nome         string
+	CargaHoraria int
+}
+
+type Cursos []Curso
+
+func main() {
+	templates := []string{
+		"header.html",
+		"content.html",
+		"footer.html",
+	}
+	t := template.Must(template.New("content.html").ParseFiles(templates...))
+	err := t.Execute(os.Stdout, Cursos{
+		{"Go", 40},
+		{"Python", 20},
+		{"Java", 30},
+		{"TypeScript", 50},
+	})
+	if err != nil {
+		panic(err)
+	}
+}
