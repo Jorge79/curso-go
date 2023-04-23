50070a195074246052db3c7564aafcec856dbff1
Jorge79 <Jorge7.9@hotmail.com>
2023-04-23 19:42:36 -0300

    customizando request

---
+++ cap2/templates/9/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"io/ioutil"
+	"net/http"
+)
+
+func main() {
+	c := http.Client{}
+	req, err := http.NewRequest("GET", "https://www.google.com", nil)
+	if err != nil {
+		panic(err)
+	}
+	req.Header.Add("Accept", "application/json")
+	resp, err := c.Do(req)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+	println(string(body))
+}
