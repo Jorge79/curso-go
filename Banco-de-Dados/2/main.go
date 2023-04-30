bf8127de3f8104b2e51802d906fad18964d1b212
Jorge79 <Jorge7.9@hotmail.com>
2023-04-30 18:42:34 -0300

    soft delete

--- Banco-de-Dados/2/main.go
+++ Banco-de-Dados/2/main.go
@@ -1,27 +1,32 @@
 package main
 
 import (
-	"fmt"
-
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
+type Catergory struct {
+	ID   int `gorm:"primaryKey"`
+	Name string
+	gorm.Model
+}
+
 type Product struct {
-	ID    string `gorm:"primary_key"`
+	ID    int `gorm:"primaryKey"`
 	Name  string
 	Price float64
+	gorm.Model
 }
 
 func main() {
-	dsn := "root:root@tcp(localhost:3306)/curso-go"
+	dsn := "root:root@tcp(localhost:3306)/curso-go?charset=utf8mb4&parseTime=True&loc=Local"
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		panic("failed to connect database")
 	}
 	db.AutoMigrate(&Product{})
 
-	// db.Create(&Product{Name: "Notebook", Price: 2})
+	// db.Create(&Product{Name: "Notebook", Price: 1000})
 
 	// //create Batch
 	// products := db.Create(&[]Product{
@@ -51,13 +56,14 @@ func main() {
 	// for _, product := range products {
 	// 	fmt.Println(product)
 
-	var p Product
-	db.First(&p, 1)
-	p.Name = "New Monitor"
-	db.Save(&p)
+	// var p Product
+	// db.First(&p, 1)
+	// p.Name = "New Monitor"
+	// p.Price = 500
+	// db.Save(&p)
 
-	var p2 Product
-	db.First(&p2, 2)
-	fmt.Println(p2.Name)
-
+	// var p2 Product
+	// db.First(&p2, 3)
+	// fmt.Println(p2.Name)
+	// db.Delete(&p2)
 }
