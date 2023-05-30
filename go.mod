d7d4b38600df1184ed2fe52729e1743f60468dd9
Jorge79 <Jorge7.9@hotmail.com>
2023-05-30 13:58:10 -0300

    acessa pacotes criados

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Jorge79/curso-go/packaging/1
+
+go 1.20
---
+++ packaging/1/cmd/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/Jorge79/curso-go/packaging/1/math"
+)
+
+func main() {
+	fmt.Println("Hello, World!")
+
+	m := math.Math{A: 1, B: 2}
+	fmt.Println(m.Add())
+}
