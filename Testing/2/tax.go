1f4114f3adb2559541e81ffce73055c9f08b16f3
Jorge79 <Jorge7.9@hotmail.com>
2023-06-04 18:16:43 -0300

    Mocks

--- Testing/2/tax.go
+++ Testing/2/tax.go
@@ -2,6 +2,15 @@ package tax
 
 import "errors"
 
+type Repository interface {
+	SaveTax(amount float64) error
+}
+
+func CalculateTaxAndSave(amount float64, repository Repository) error {
+	tax := calculateTax2(amount)
+	return repository.SaveTax(tax)
+}
+
 func calculateTax(amount float64) (float64, error) {
 	if amount <= 0 {
 		return 0, errors.New("amount must be greater than zero")
@@ -16,4 +25,20 @@ func calculateTax(amount float64) (float64, error) {
 	}
 
 	return 5.0, nil
+}
+
+func calculateTax2(amount float64) float64 {
+	if amount <= 0 {
+		return 0
+	}
+
+	if amount >= 1000 && amount < 20000 {
+		return 10.0
+	}
+
+	if amount >= 20000 {
+		return 20.0
+	}
+
+	return 5.0
 }
--- Testing/2/tax_test.go
+++ Testing/2/tax_test.go
@@ -1,8 +1,10 @@
 package tax
 
 import (
-	"github.com/stretchr/testify/assert"
+	"errors"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestCalculateTax(t *testing.T) {
@@ -13,5 +15,20 @@ func TestCalculateTax(t *testing.T) {
 	tax, err = calculateTax(0)
 	assert.Error(t, err, "amount must be greater than zero")
 	assert.Equal(t, 0.0, tax)
-	assert.Contains(t, err.Error(), " greater than zero")
+	assert.Contains(t, err.Error(), "greater than zero")
+}
+
+func TestCalculateTaxAndSave(t *testing.T) {
+	repository := &TaxRepositoryMock{}
+	repository.On("SaveTax", 10.0).Return(nil)
+	repository.On("SaveTax", 0.0).Return(errors.New("error saving tax"))
+
+	err := CalculateTaxAndSave(1000.0, repository)
+	assert.Nil(t, err)
+
+	err = CalculateTaxAndSave(0.0, repository)
+	assert.Error(t, err, "error saving tax")
+
+	repository.AssertExpectations(t)
+	repository.AssertNumberOfCalls(t, "SaveTax", 2)
 }
