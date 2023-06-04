35291afd4da665ce7927d1b7761ee7320432d3aa
Jorge79 <Jorge7.9@hotmail.com>
2023-06-03 22:52:21 -0300

    Fuzzing

--- Testing/1/tax.go
+++ Testing/1/tax.go
@@ -3,15 +3,19 @@ package tax
 import "time"
 
 func calculateTax(amount float64) float64 {
-	if amount == 0 {
+	if amount <= 0 {
 		return 0
 	}
 
-	if amount >= 1000 {
-		return 10
-	} else {
-		return 5
+	if amount >= 1000 && amount < 20000 {
+		return 10.0
 	}
+
+	if amount >= 20000 {
+		return 20.0
+	}
+
+	return 5.0
 }
 
 func calculateTax2(amount float64) float64 {
--- Testing/1/tax_test.go
+++ Testing/1/tax_test.go
@@ -19,7 +19,6 @@ func TestCalculateTaxBatch(t *testing.T) {
 	}
 
 	table := []calcTax{
-		{0, 0},
 		{500, 5},
 		{1000, 10},
 		{2000, 10},
@@ -43,4 +42,20 @@ func BenchmarkCalculateTax2(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		calculateTax2(500)
 	}
+}
+func FuzzCalculateTax(f *testing.F) {
+	seed := []float64{-1, -2, -2.5, 500.0, 1000.0, 1501.0}
+	for _, amount := range seed {
+		f.Add(amount)
+	}
+	f.Fuzz(func(t *testing.T, amount float64) {
+		result := calculateTax(amount)
+		if amount <= 0 && result != 0 {
+			t.Errorf("Received %f; expected 0", result)
+		}
+
+		if amount > 20000 && result != 20 {
+			t.Errorf("Received %f; expected 20", result)
+		}
+	})
 }
