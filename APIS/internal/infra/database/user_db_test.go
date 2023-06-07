4d33d45c9d319fe12b2872db7ab004ea6384fa99
Jorge79 <Jorge7.9@hotmail.com>
2023-06-06 23:50:24 -0300

    test User creation

---
+++ APIS/internal/infra/database/user_db_test.go
@@ -0,0 +1,38 @@
+package database
+
+import (
+	"testing"
+
+	"github.com/Jorge79/estudos-go/APIS/internal/entity"
+	"github.com/stretchr/testify/assert"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+func TestCreateUser(t *testing.T) {
+	db, err := gorm.Open(sqlite.Open("file::memory:"), &gorm.Config{})
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	db.AutoMigrate(&entity.User{})
+
+	user, _ := entity.NewUser(
+		"John Doe",
+		"test@gmail.com",
+		"123456",
+	)
+
+	userDB := NewUser(db)
+	err = userDB.Create(user)
+	assert.Nil(t, err)
+
+	var userFound entity.User
+	err = db.First(&userFound, "id = ?", user.ID).Error
+	assert.Nil(t, err)
+	assert.Equal(t, user.ID, userFound.ID)
+	assert.Equal(t, user.Name, userFound.Name)
+	assert.Equal(t, user.Email, userFound.Email)
+	assert.NotNil(t, userFound.Password)
+}
