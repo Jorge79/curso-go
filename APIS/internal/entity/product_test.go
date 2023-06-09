d92dfbaa68ce7ac93c9c5f2b94dcc9aca39301df
Jorge79 <Jorge7.9@hotmail.com>
2023-06-08 21:53:18 -0300

    test ProductDB

--- APIS/internal/entity/product_test.go
+++ APIS/internal/entity/product_test.go
@@ -7,16 +7,16 @@ import (
 )
 
 func TestNewProduct(t *testing.T) {
-	p, err := NewProduct("Product 1", 100)
+	p, err := NewProduct("Product 1", 100.0)
 	assert.Nil(t, err)
 	assert.NotNil(t, p)
 	assert.NotEmpty(t, p.ID)
 	assert.Equal(t, "Product 1", p.Name)
-	assert.Equal(t, 100, p.Price)
+	assert.Equal(t, 100.0, p.Price)
 }
 
 func TestProductWhenNameIsRequired(t *testing.T) {
-	p, err := NewProduct("", 10)
+	p, err := NewProduct("", 10.0)
 	assert.Nil(t, p)
 	assert.Equal(t, ErrNameIsRequired, err)
 }
@@ -28,13 +28,13 @@ func TestProductWhenPriceIsRequired(t *testing.T) {
 }
 
 func TestProductWhenPriceIsInvalid(t *testing.T) {
-	p, err := NewProduct("Product 1", -10)
+	p, err := NewProduct("Product 1", -10.0)
 	assert.Nil(t, p)
 	assert.Equal(t, ErrInvalidPrice, err)
 }
 
 func TestProductValidade(t *testing.T) {
-	p, err := NewProduct("Product 1", 10)
+	p, err := NewProduct("Product 1", 10.0)
 	assert.Nil(t, err)
 	assert.NotNil(t, p)
 	assert.Nil(t, p.Validate())
