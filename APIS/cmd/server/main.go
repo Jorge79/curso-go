1f8c2d39db48aec2d39a0f20398af919be24898f
Jorge79 <Jorge7.9@hotmail.com>
2023-06-10 23:03:22 -0300

    add go-chi

--- APIS/cmd/server/main.go
+++ APIS/cmd/server/main.go
@@ -7,6 +7,8 @@ import (
 	"github.com/Jorge79/estudos-go/APIS/internal/entity"
 	"github.com/Jorge79/estudos-go/APIS/internal/infra/database"
 	"github.com/Jorge79/estudos-go/APIS/internal/infra/webserver/handlers"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 )
@@ -24,7 +26,10 @@ func main() {
 
 	productDB := database.NewProduct(db)
 	productHandler := handlers.NewProductHandler(productDB)
-	http.HandleFunc("/products", productHandler.CreateProduct)
 
-	http.ListenAndServe(":8000", nil)
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Post("/products", productHandler.CreateProduct)
+
+	http.ListenAndServe(":8000", r)
 }
