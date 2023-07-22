f136939ffc09b6b55faa64b683b689ebd438e4fa
Jorge79 <Jorge7.9@hotmail.com>
2023-07-22 16:24:27 -0300

    upload function

--- uploadS3/cmd/uploader/main.go
+++ uploadS3/cmd/uploader/main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"fmt"
+	"os"
+
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/credentials"
 	"github.com/aws/aws-sdk-go/aws/session"
@@ -32,4 +35,24 @@ func init() {
 
 func main() {
 
+}
+
+func uploadFile(filename string) {
+  completeFileName := fmt.Sprintf("./tmp/file%s", filename)
+  f, err := os.Open("Uploading file %s to bucket %s",completeFileName, s3Bucket)
+  if err != nil {
+    fmt.Printf("Error opnening file %s\n", completeFileName)
+    return
+  }
+  defer f.Close()
+  _, err = s3Client.PutObject(&s3.PutObjectInput{
+    Bucket: aws.String(s3Bucket),
+    Key: aws.String(filename),
+    Body: f,
+  })
+  if err != nil {
+    fmt.Printf("Error uploading file %s\n", completeFileName)
+    return
+  }
+  fmt.Printf("File %s uploaded succesfully\n", completeFileName)
 }
