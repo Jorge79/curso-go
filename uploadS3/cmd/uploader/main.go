9dbef3faa4d21a505adf1703349e265deb3fa580
Jorge79 <Jorge7.9@hotmail.com>
2023-07-23 18:08:09 -0300

    go routine and limit upload

--- uploadS3/cmd/uploader/main.go
+++ uploadS3/cmd/uploader/main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"os"
+	"sync"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/credentials"
@@ -13,46 +15,72 @@ import (
 var (
 	s3Client *s3.S3
 	s3Bucket string
+	wg       sync.WaitGroup
 )
 
 func init() {
 	sess, err := session.NewSession(
-    &aws.Config{
-      Region: aws.String("us-east-1")
-      Credentials: credentials.NewStaticCredentials(
-        "EXEMPLOTESTE",
-        "EXEMPLOKEY",
-        "",
-      ),
-    },
-  )
-  if err != nil {
-    panic(err)
-  }
-  s3Client = s3.New(sess)
-  s3Bucket = "my-bucket-exemplo"
+		&aws.Config{
+			Region: aws.String("us-east-1"),
+			Credentials: credentials.NewStaticCredentials(
+				"AKIARW42GOKUCACZUCG6",
+				"X6UZ7kcBJTMYgFO3/0MfwFAdhS+QHSzcrL3kfpdF",
+				"",
+			),
+		},
+	)
+	if err != nil {
+		panic(err)
+	}
+	s3Client = s3.New(sess)
+	s3Bucket = "curso-go-bucket"
 }
 
 func main() {
-
+	dir, err := os.Open("./tmp")
+	if err != nil {
+		panic(err)
+	}
+	defer dir.Close()
+	uploadControl := make(chan struct{}, 100)
+	for {
+		files, err := dir.ReadDir(1)
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			fmt.Printf("Error reading directory: %s\n", err)
+			continue
+		}
+		wg.Add(1)
+		uploadControl <- struct{}{}
+		go uploadFile(files[0].Name(), uploadControl)
+	}
+	wg.Wait()
 }
 
-func uploadFile(filename string) {
-  completeFileName := fmt.Sprintf("./tmp/file%s", filename)
-  f, err := os.Open("Uploading file %s to bucket %s",completeFileName, s3Bucket)
-  if err != nil {
-    fmt.Printf("Error opnening file %s\n", completeFileName)
-    return
-  }
-  defer f.Close()
-  _, err = s3Client.PutObject(&s3.PutObjectInput{
-    Bucket: aws.String(s3Bucket),
-    Key: aws.String(filename),
-    Body: f,
-  })
-  if err != nil {
-    fmt.Printf("Error uploading file %s\n", completeFileName)
-    return
-  }
-  fmt.Printf("File %s uploaded succesfully\n", completeFileName)
+func uploadFile(filename string, uploadControl <-chan struct{}) {
+	defer wg.Done()
+	completeFileName := fmt.Sprintf("./tmp/%s", filename)
+	fmt.Printf("Uploading file %s to bucket %s\n", completeFileName, s3Bucket)
+	f, err := os.Open(completeFileName)
+	if err != nil {
+		fmt.Printf("Error opnening file %s\n", completeFileName)
+		<-uploadControl
+		println(err)
+		return
+	}
+	defer f.Close()
+	_, err = s3Client.PutObject(&s3.PutObjectInput{
+		Bucket: aws.String(s3Bucket),
+		Key:    aws.String(filename),
+		Body:   f,
+	})
+	if err != nil {
+		fmt.Printf("Error uploading file %s\n", completeFileName)
+		<-uploadControl
+		return
+	}
+	fmt.Printf("File %s uploaded succesfully\n", completeFileName)
+	<-uploadControl
 }
