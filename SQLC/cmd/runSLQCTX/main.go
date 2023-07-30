6a499455c85459b04bf1ed2ad6b0530b02d3b2ee
Jorge79 <Jorge7.9@hotmail.com>
2023-07-29 23:55:58 -0300

    transactions

---
+++ SQLC/cmd/runSLQCTX/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+
+	"github.com/Jorge79/estudos-go/SQLC/internal/db"
+	_ "github.com/go-sql-driver/mysql"
+)
+
+type CourseDB struct {
+	dbConn *sql.DB
+	*db.Queries
+}
+
+func NewCourseDB(dbConn *sql.DB) *CourseDB {
+	return &CourseDB{
+		dbConn:  dbConn,
+		Queries: db.New(dbConn)}
+}
+
+func (c *CourseDB) callTx(ctx context.Context, fn func(*db.Queries) error) error {
+	tx, err := c.dbConn.BeginTx(ctx, nil)
+	if err != nil {
+		return err
+	}
+
+	q := db.New(tx)
+	err = fn(q)
+	if err != nil {
+		if errRB := tx.Rollback(); errRB != nil {
+			return fmt.Errorf("error on rollback: %v, original error %v", errRB, err)
+		}
+		return err
+	}
+	return tx.Commit()
+}
+
+func main() {
+	ctx := context.Background()
+	dbConn, err := sql.Open("mysql", "root:root@tcp(localhost:3306)/courses")
+	if err != nil {
+		panic(err)
+	}
+	defer dbConn.Close()
+
+	queries := db.New(dbConn)
+
+}
